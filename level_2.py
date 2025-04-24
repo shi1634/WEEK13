57476ece48bce46ba0a9bf44eacb76f03d756014
席少棠 <xishaotang@xishaotangdeMacBook-Pro.local>
2025-04-24 08:56:29 -0400

    Level 2 Complete

---
+++ level_2.py
@@ -0,0 +1,11 @@
+mix_of_stuff = ["hello", 42, 3.14, True]
+playlist_for_coding = [
+    "Dolphin by Tennyson",
+    "I Found My Smile Again by D'Angelo",
+    "Love Theory by Kirk Franklin",
+    "I Wish I Knew by Blue Mitchell",
+    "Mercy Mercy Me by Marvin Gaye"
+]
+print(playlist_for_coding[0])
+
+playlist_for_coding.append("Shut up and dance by walk the moon")
